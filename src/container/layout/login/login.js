739b1b5ac694c4f9a222eec9d2a5c7085d945a46
ngocmai-26 <80842972+ngocmai-26@users.noreply.github.com>
2023-07-21 23:29:23 +0700

    UI Login and Register

---
+++ src/container/layout/login/login.js
@@ -0,0 +1,83 @@
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputLabel,
+  Link,
+} from '@mui/material'
+
+import { LoginLayout } from './style'
+import LoginImage from '../../../assets/images/Rectangle121.png'
+
+function Login() {
+  return (
+    <LoginLayout>
+      <div className="layoutItem">
+        <div className="layoutItem-left">
+          <img
+            src={LoginImage}
+            className="layoutItem-img"
+            height="100%"
+            width="100%"
+          ></img>
+        </div>
+
+        <div className="layoutItem-right">
+          <FormControl width="100%">
+            {/* <InputLabel >Email address</InputLabel>
+            <Input  />
+            <FormHelperText id="my-helper-text">
+              We'll never share your email.
+            </FormHelperText> */}
+            <div className="layoutItem-title">Đăng nhập</div>
+            <div className="layoutItem-description">
+              Đăng nhập để trải nghiệm dịch vụ đặt vé xem phim ở bất kì đâu
+            </div>
+            <div className="username layoutLogin-item">
+              <FormLabel className="login_label">
+                Tên đăng nhập <span className="note">*</span>
+              </FormLabel>
+              <input
+                name="username"
+                id="username"
+                className="login_input"
+                type="text"
+                required
+              ></input>
+            </div>
+
+            <div className="password layoutLogin-item">
+              <FormLabel className="login_label">
+                Mật khẩu <span className="note">*</span>
+              </FormLabel>
+              <input
+                name="password"
+                id="password"
+                className="login_input"
+                type="text"
+                required
+              ></input>
+            </div>
+
+            <div className="layoutLogin-item layoutLogin-memorize">
+              <div className="memorize-password">
+                <input type="checkbox"></input>
+                <FormLabel>Nhớ mật khẩu</FormLabel>
+              </div>
+
+              <Link className="forgot-password">Quên mật khẩu?</Link>
+            </div>
+
+            <Button variant="contained" className="btn-Submit" type="submit">
+              Đăng nhập
+            </Button>
+          </FormControl>
+        </div>
+      </div>
+    </LoginLayout>
+  )
+}
+
+export default Login
---
+++ src/container/layout/login/style.js
@@ -0,0 +1,126 @@
+import { styled } from '@mui/material/styles'
+export const LoginLayout = styled('div')`
+  background-color: #8f8f8f6b;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+
+  padding: 0px 20px;
+  z-index: 9998;
+  display: flex;
+  justify-content: center;
+    .layoutItem {
+        position: absolute;
+        background-color: #fff;
+        width: 70%;
+        min-heigh: 500px;
+        height: auto;
+        top: 13%;
+        z-index: 9999;
+        border-radius: 20px;
+        outline: none;
+        padding: 25px;
+        display: flex;
+
+        .layoutItem-left {
+            width: 55%;
+            padding:15px;
+        }
+        .layoutItem-right {
+            margin: auto;
+            width: 30%;
+            padding: 30px 25px;
+            height: 100%;
+            .layoutItem-title {
+                font-size: 40px;
+                font-weight: 600;
+                text-align: center;
+                color: #4475F2;
+                padding: 10px;
+            }
+
+            .layoutItem-description {
+                font-size: 16px;
+                text-align: center;
+                padding: 10px 0;
+            }
+
+            .layoutLogin-item {
+                width: 100%;
+                margin: 10px 0;
+                
+            }
+
+            .layoutRegister-item {
+                margin: 3px 0;
+            }
+            .login_label {
+                color: #0E1937;
+                font-weight: 500;
+                width: 100%;
+                font-size: 16px;
+                
+                .note {
+                    color: #FF3E30   
+                }
+            }
+
+            .login_input {
+                width: 95%;
+                font-size: 16px;
+                padding: 10px 10px;
+                border-radius: 30px;
+                outline: none;
+                border: 1px solid #D2D2D2;
+                color: #9C9C9C;
+                margin: 5px 0px;
+
+            }
+
+            .btn-Submit {
+                padding: 10px;
+                border-radius: 30px;
+                width: 100%;
+            }
+
+            .layoutLogin-memorize{
+                display: flex;
+                justify-content: space-between;
+                padding-bottom: 30px;
+
+                .memorize-password {
+                    margin: auto 0;
+                    label {
+                        font-size: 16px;
+                    }
+                    
+                }
+                .forgot-password {
+                    font-size: 16px;
+                    cursor: pointer;
+                    text-decoration: none;
+                }
+
+                .item-note {
+                    text-decoration: none;
+                }
+            }
+            
+        }
+
+        .layoutRegister-right {
+            padding: 0px 25px;
+        }
+
+
+    }
+    
+    .layoutRegister {
+        top: 10%;
+        padding: 25px;
+    }
+
+  }
+`
